568d427cafe0fe4dcf649bc53304c66c450e7898
Ario <ariobarxan.dev@gmail.com>
2024-12-21 14:18:04 +1100

    Delivers empty items array on 200 HTTPResponse with JSON empty list

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -56,6 +56,18 @@ final class RemoteFeedLoaderTest: XCTestCase {
         }
     }
 
+    func test_load_deliversNoItemOn200HTTPResponseWithEmptyJSONList() {
+        let (sut, client) = makeSUT()
+
+        var captureResults = [RemoteFeedLoader.Result]()
+        sut.load { captureResults.append($0)}
+
+        let emptyListJSON = Data("{\"items\": []}".utf8)
+        client.complete(withStatusCode: 200, data: emptyListJSON )
+
+        XCTAssertEqual(captureResults , [.success([])])
+    }
+
 
     // MARK: - Helpers
     private func makeSUT(withURL url: URL = URL(string: "https://www.facebook.com")!) -> (sut:RemoteFeedLoader, client: HTTPClientSpy) {
